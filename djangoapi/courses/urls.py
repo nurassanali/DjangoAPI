bde170e55e81399e2ac33395acc817252e07904d
a.nurmukhambetov <a.nurmukhambetov@ensoft.kz>
2021-01-20 21:07:15 +0600

    added import.

--- djangoapi/courses/urls.py
+++ djangoapi/courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from rest_framework import routers
+from django.urls import include
 
 router = routers.DefaultRouter()
 router.register('courses', views.CourseView)
